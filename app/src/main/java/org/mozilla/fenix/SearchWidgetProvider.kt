53863ce13215c38ab17c94fe9160bd33125f10fe
Yeon Taek Jeong <yeontaekj@gmail.com>
2019-07-17 12:50:46 -0700

    Add different sized widgets

--- app/src/main/java/org/mozilla/fenix/SearchWidgetProvider.kt
+++ app/src/main/java/org/mozilla/fenix/SearchWidgetProvider.kt
@@ -6,32 +6,77 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
+import org.jetbrains.anko.layoutInflater
 
-class SearchWidgetProvider : AppWidgetProvider() {
-
+class SearchWidgetOneButtonProvider : AppWidgetProvider() {
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+
+        val pendingIntent: PendingIntent = Intent(context, HomeActivity::class.java)
+            .let { intent ->
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                intent.putExtra(HomeActivity.OPEN_TO_SEARCH, true)
+                PendingIntent.getActivity(context, 0, intent, 0)
+            }
+
+        val views: RemoteViews = RemoteViews(context.packageName, R.layout.search_widget_one_button)
+            .apply {
+                setOnClickPendingIntent(R.id.button_search_widget_new_tab, pendingIntent)
+            }
+
         appWidgetIds.forEach { appWidgetId ->
-            val pendingIntent: PendingIntent = Intent(context, HomeActivity::class.java)
-                .let { intent ->
-                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                    intent.putExtra(HomeActivity.OPEN_TO_SEARCH, true)
-                    PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT)
-                }
-
-            val pendingIntent2: PendingIntent = Intent(context, IntentReceiverActivity::class.java)
-                .let { intent ->
-                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                    intent.putExtra(IntentReceiverActivity.SPEECH_PROCESSING, true)
-                    PendingIntent.getActivity(context, 1, intent, PendingIntent.FLAG_CANCEL_CURRENT)
-                }
-
-            val views: RemoteViews = RemoteViews(context.packageName, R.layout.search_widget)
-                .apply {
-                    setOnClickPendingIntent(R.id.button, pendingIntent)
-                    setOnClickPendingIntent(R.id.button2, pendingIntent2)
-                }
-
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
+}
+
+open class SearchWidgetTwoButtonsProvider(val identifier: SearchWidgetIdentifier) : AppWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        val (text, layout) = when (identifier) {
+            SearchWidgetIdentifier.SMALL -> Pair("", R.layout.search_widget_small)
+            SearchWidgetIdentifier.MEDIUM -> Pair("Search", R.layout.search_widget_medium)
+            SearchWidgetIdentifier.LARGE -> Pair("Search the web", R.layout.search_widget_large)
+            SearchWidgetIdentifier.EXTRA_LARGE -> Pair("Search the web", R.layout.search_widget_extra_large)
+        }
+
+        val pendingIntent: PendingIntent = Intent(context, HomeActivity::class.java)
+            .let { intent ->
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                intent.putExtra(HomeActivity.OPEN_TO_SEARCH, true)
+                PendingIntent.getActivity(context, 0, intent, 0)
+            }
+
+        val pendingIntent2: PendingIntent = Intent(context, IntentReceiverActivity::class.java)
+            .let { intent ->
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                intent.putExtra(IntentReceiverActivity.SPEECH_PROCESSING, true)
+                PendingIntent.getActivity(context, 1, intent, 0)
+            }
+
+        val views: RemoteViews = RemoteViews(context.packageName, layout)
+            .apply {
+                setTextViewText(R.id.text_search_widget, text)
+                setOnClickPendingIntent(R.id.button_search_widget_new_tab, pendingIntent)
+                setOnClickPendingIntent(R.id.button_search_widget_voice, pendingIntent2)
+            }
+
+        appWidgetIds.forEach { appWidgetId ->
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+    fun adjustLayoutSize() {
+
+    }
+}
+class SearchWidgetProviderExtraLarge : SearchWidgetTwoButtonsProvider(identifier = SearchWidgetIdentifier.EXTRA_LARGE)
+
+class SearchWidgetProviderSmall : SearchWidgetTwoButtonsProvider(identifier = SearchWidgetIdentifier.SMALL)
+class SearchWidgetProviderMedium : SearchWidgetTwoButtonsProvider(identifier = SearchWidgetIdentifier.MEDIUM)
+class SearchWidgetProviderLarge : SearchWidgetTwoButtonsProvider(identifier = SearchWidgetIdentifier.LARGE)
+
+enum class SearchWidgetIdentifier {
+    SMALL,
+    MEDIUM,
+    LARGE,
+    EXTRA_LARGE
 }
